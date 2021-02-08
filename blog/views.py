468bd8e6a8a68eb2338cd04d12329693e2fd352b
Mahmoud Halim <mahmoudahalim0@gmail.com>
2021-02-08 16:07:57 +0200

    add home and about pages

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
 
-# Create your views here.
+posts = [
+    {
+        'author': 'John Doe',
+        'title': 'Blog Post 1',
+        'content': 'First blog post!',
+        'date_posted': '8/2/2021'
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'Blog Post 2',
+        'content': 'Second blog post!',
+        'date_posted': '20/2/2021'
+    }
+]
+
+
+def home(request):
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
+
+
+def about(request):
+    return render(request, 'blog/about.html', {'title': 'About'})
