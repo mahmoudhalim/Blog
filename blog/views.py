24b62bccd3db9fde9687ed40bac4b8af0358b9ea
Mahmoud Halim <mahmoudahalim0@gmail.com>
2021-02-09 00:50:23 +0200

    update Posts

--- blog/views.py
+++ blog/views.py
@@ -1,24 +1,10 @@
 from django.shortcuts import render
-
-posts = [
-    {
-        'author': 'John Doe',
-        'title': 'Blog Post 1',
-        'content': 'First blog post!',
-        'date_posted': '8/2/2021'
-    },
-    {
-        'author': 'Jane Doe',
-        'title': 'Blog Post 2',
-        'content': 'Second blog post!',
-        'date_posted': '20/2/2021'
-    }
-]
+from .models import Post
 
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
